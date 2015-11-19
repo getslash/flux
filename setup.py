fe3143977c949a3baac2edceabc0a9e68975a666
Rotem Yaari <vmalloc@gmail.com>
2015-11-19 09:10:14 +0200

    Fix typo

--- setup.py
+++ setup.py
@@ -2,12 +2,9 @@ import os
 from setuptools import setup, find_packages
 
 with open(os.path.join(os.path.dirname(__file__), "flux", "__version__.py")) as version_file:
-    exec(version_file.read()) # pylint: disable=W0122
+    exec(version_file.read())  # pylint: disable=W0122
 
 setup(name="flux",
-      classifiers = [
-          "Programming Language :: Python :: 2.7",
-          ],
       description="Artificial time library",
       license="BSD",
       author="Rotem Yaari",
@@ -20,7 +17,7 @@ setup(name="flux",
           "Programming Language :: Python :: 3.5",
       ],
 
-      version=__version__, # pylint: disable=E0602
+      version=__version__,  # pylint: disable=E0602
       packages=find_packages(exclude=["tests"]),
       install_requires=[],
       scripts=[],
