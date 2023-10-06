cc42e3c414fa8b07cdb00a1dbb91771fee4743c3
Ayala Shachar <shachar.ayala@gmail.com>
2023-10-06 11:37:54 +0300

    Remove usage of deprecated API 'pkg_resources'

--- flux/__version__.py
+++ flux/__version__.py
@@ -1,3 +1,10 @@
-import pkg_resources
+import sys
 
-__version__ = pkg_resources.get_distribution('flux').version
+if sys.version_info < (3, 8):
+    import pkg_resources
+    get_distribution = pkg_resources.get_distribution
+else:
+    from importlib.metadata import distribution
+    get_distribution = distribution
+
+__version__ = get_distribution('flux').version
